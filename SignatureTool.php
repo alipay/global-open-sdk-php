057a24ed77bcc2f883577a4bd9f52d86a9aa9b77
songlin <songlin.xiesl@alibaba-inc.com>
2020-02-04 17:00:44 +0800

    Compatible with Windows system

--- SignatureTool.php
+++ SignatureTool.php
@@ -14,22 +14,23 @@ class SignatureTool{
     }
 
     static private function genSignContent($httpMethod, $path, $clientId, $timeString, $content){
-        $payload = $httpMethod . " " . $path . PHP_EOL . $clientId . "." . $timeString . "." . $content;
+        $payload = $httpMethod . " " . $path . "\n" . $clientId . "." . $timeString . "." . $content;
         return $payload;
     }
 
     static private function signWithSHA256RSA($signContent, $merchantPrivateKey){
-        $priKey = "-----BEGIN RSA PRIVATE KEY-----" . PHP_EOL .
-            wordwrap($merchantPrivateKey, 64, PHP_EOL, true) .
-            PHP_EOL . "-----END RSA PRIVATE KEY-----";
+        $priKey = "-----BEGIN RSA PRIVATE KEY-----\n" .
+            wordwrap($merchantPrivateKey, 64, "\n", true) .
+            "\n-----END RSA PRIVATE KEY-----";
+
         openssl_sign($signContent, $signValue, $priKey, OPENSSL_ALGO_SHA256);
         return base64_encode($signValue);
     }
 
     static private function verifySignatureWithSHA256RSA($rspContent, $rspSignValue, $alipayPublicKey){
-        $pubKey = "-----BEGIN PUBLIC KEY-----" . PHP_EOL .
-            wordwrap($alipayPublicKey, 64, PHP_EOL, true) .
-            PHP_EOL . "-----END PUBLIC KEY-----";
+        $pubKey = "-----BEGIN PUBLIC KEY-----\n" .
+            wordwrap($alipayPublicKey, 64, "\n", true) .
+            "\n-----END PUBLIC KEY-----";
         $originalRspSignValue = base64_decode(urldecode($rspSignValue));
         $verifyResult = openssl_verify($rspContent, $originalRspSignValue, $pubKey, OPENSSL_ALGO_SHA256);
         return $verifyResult;
